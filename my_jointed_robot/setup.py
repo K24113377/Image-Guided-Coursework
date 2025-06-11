25b04aee809a57fb40f0625df29753bc068c8de2
K24113377 <k24113377@kcl.ac.uk>
2025-06-11 18:49:48 +0100

    Add files via upload

---
+++ my_jointed_robot/setup.py
@@ -0,0 +1,23 @@
+from setuptools import find_packages, setup
+from glob import glob
+
+package_name = 'my_jointed_robot'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name + '/urdf', glob('urdf/*')),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='Your Name',
+    maintainer_email='your.email@example.com',
+    description='My jointed robot description package',
+    license='MIT',
+    tests_require=['pytest'],
+    entry_points={},
+)
